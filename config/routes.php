f3fe4d2b72f1c9571033927a49d4858a2bde2bc0
Rony <ronypc@outlook.com>
2020-06-14 14:44:52 -0300

    mudancas

--- config/routes.php
+++ config/routes.php
@@ -5,11 +5,7 @@ use Alura\Phpweb\Controller\{
     Exclusao,
     FormularioDeEdicao,
     FormularioDeInsercao,
-<<<<<<< HEAD
-    FormularioLogin,
-=======
     Formulariologin,
->>>>>>> 9fcd2639307468b1081735342edcb519aadef704
     ListarCursos,
     Persistencia,
     RealizarLogin
@@ -21,11 +17,7 @@ return [
     '/salvar-curso' => Persistencia::class,
     '/excluir-curso' => Exclusao::class,
     '/alterar-curso' => FormularioDeEdicao::class,
-<<<<<<< HEAD
-    '/login' => FormularioLogin::class,
-=======
     '/login' => Formulariologin::class,
     '/realiza-login' => RealizarLogin::class,
     '/logout' => Deslogar::class,
->>>>>>> 9fcd2639307468b1081735342edcb519aadef704
 ];
