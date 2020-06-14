105c6e1221d4cc04e28be1e92e00b03bc68ef97e
Rony <ronypc@outlook.com>
2020-06-14 15:08:02 -0300

    sdas

--- view/login/formulario.php
+++ view/login/formulario.php
@@ -1,23 +1,4 @@
 <?php require __DIR__ . '/../inicio-html.php'; ?>
-<<<<<<< HEAD
-
-<form action="/realiza-login" method="POST">
-    <div class="form-group">
-        <label for="email">E-mail:</label>
-        <input type="email" name="email" id="email" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha"id="senha" class="form-control">
-
-    </div>
-    <button class="btn btn-primary btn-sm btn-block">
-        Login
-    </button>
-</form>
-
-
-=======
 <form action="/realiza-login" method="post">
     <div class="form-group">
         <label for="email">E-mail</label>
@@ -30,5 +11,4 @@
     <button class="btn btn-primary">Login</button>
 </form>
 
->>>>>>> 9fcd2639307468b1081735342edcb519aadef704
 <?php require __DIR__ . '/../fim-html.php'; ?>
